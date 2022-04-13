91d6241226aa2ebe81af9ff7eb5ca9474c39494e
eek <emremrklc@gmail.com>
2022-04-13 09:02:36 +0300

    bug fixes

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-const https = require('https');
-url = `https://api.qrserver.com/v1/create-qr-code/?format=eps?size=150x150&data=Example`;
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios').default;
 
-https.get(url, (response) => {
-    let data = ''
+const downloadFile = async (fileUrl, downloadFolder) => {
+    const fileName = path.basename(fileUrl);
+    const localFilePath = path.resolve(__dirname, downloadFolder, fileName);
 
-    response.on('data', (chunk) =>{
-        data = data + chunk;
-    });
+    try{
+        const response = await axios({
+            method: 'GET',
+            url: fileUrl,
+            responseType: 'stream'
+        });
 
-    response.on('end', () =>{
-        //console.log(data);
-        console.log("seviselim mi ada?");
-    });
-}).on('error', (err) =>{
-    console.log(`Error: ${err.message}`);
-});
+        const w = response.data.pipe(fs.createWriteStream(localFilePath));
+        w.on('finish', () => {
+            console.log(`yeah boi`);
+        });
+    } catch (err) {
+        throw new Error(err);
+    }
+};
+
+downloadFile('https://api.qrserver.com/v1/create-qr-code/?data=HelloWorld&amp;size=100x100', 'download');
